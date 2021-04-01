82cf84640a6a0fe74604ef9982d9db846a960aa5
bhuvi <bhuvaneswaran@zetwerk.com>
2021-04-01 18:57:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChildren, QueryList, ChangeDetectorRef } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList} from '@angular/cdk/drag-drop';
+import { tap, startWith } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'inbetween';
+  slides = [{ color : '#605f8f', overlay : []} , { color : '#ff50a0', overlay : []}, { color : '#f3aa51', overlay : []}, { color : '#fcf695', overlay : []}, { color : '#567ace', overlay : []}, { color : '#b7d3e9', overlay : []}];
+  playArea = [];
+  sketches = [{ image : 'google.jpg'}, { image : 'facebook.jpg'}, { image : 'insta.jpg'}, { image : 'twitter.jpg'}, { image : 'youtube.jpg'}];
+
+  @ViewChildren('item', { read: CdkDropList })
+  slideList: QueryList<CdkDropList>;
+
+  addedSlides: CdkDropList[] = [];
+
+  constructor(private cd: ChangeDetectorRef) {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.slideList.changes.pipe(
+      startWith(true),
+      tap(() => {
+        this.addedSlides = this.slideList.toArray();
+        this.cd.markForCheck();
+        this.cd.detectChanges();
+        console.log(this.addedSlides);
+      }),
+    ).subscribe();
+  }
+
+  drop(event: CdkDragDrop<number[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
+  isSlidePredicate(item: CdkDrag<number>) {
+    return !!item.data['color'];
+  }
+
+  isSketchPredicate(item: CdkDrag<number>) {
+    return !!item.data['image'];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {DragDropModule} from '@angular/cdk/drag-drop';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SlideComponent } from './components/slide/slide.component';
+import { SketchComponent } from './components/sketch/sketch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideComponent,
+    SketchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
